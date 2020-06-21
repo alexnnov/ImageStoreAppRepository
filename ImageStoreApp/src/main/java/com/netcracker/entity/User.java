39e8734c6846a779a1596ab6fe6561cf94aa6f7e
alexnnov <45225849+alexnnov@users.noreply.github.com>
2020-06-21 19:19:31 +0300

    Added and tested account functionality and email sending

--- ImageStoreApp/src/main/java/com/netcracker/entity/User.java
+++ ImageStoreApp/src/main/java/com/netcracker/entity/User.java
@@ -37,14 +37,66 @@ public class User implements UserDetails{
 	private String lastName;
 	
 	@Column(name="email", nullable = false, updatable = false)
-	public String email;
-	public String phone;
-	public boolean enabled=true;
+	private String email;
+	private String phone;
+	private boolean enabled=true;
 	
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JsonIgnore
 	private Set<UserRole> userRoles = new HashSet<>();
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		Set<GrantedAuthority> authorites = new HashSet<>();
@@ -52,7 +104,6 @@ public class User implements UserDetails{
 		
 		return authorites;
 	}
-	
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
@@ -73,17 +124,6 @@ public class User implements UserDetails{
 	public boolean isEnabled() {
 		return enabled;
 	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	
-	
-	public String getUsername() {
-		return username;
-	}
-
 		
 		
 
--- ImageStoreApp/src/main/java/com/netcracker/service/impl/UserServiceImpl.java
+++ ImageStoreApp/src/main/java/com/netcracker/service/impl/UserServiceImpl.java
@@ -1,16 +1,33 @@
 package com.imagestore.service.impl;
 
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.imagestore.domain.User;
 import com.imagestore.domain.security.PasswordResetToken;
+import com.imagestore.domain.security.UserRole;
 import com.imagestore.repository.PasswordResetTokenRepository;
+import com.imagestore.repository.RoleRepository;
+import com.imagestore.repository.UserRepository;
 import com.imagestore.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
-	@Autowired PasswordResetTokenRepository passwordResetTokenRepository;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordResetTokenRepository passwordResetTokenRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Override
 	public PasswordResetToken getPasswordResetToken(final String token) {
@@ -19,9 +36,40 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public void createPasswordResetTokenForUser(final User user, final String token) {
-		final PasswordResetToken myToken = new PasswordResetToken(token,user);
+		final PasswordResetToken myToken = new PasswordResetToken(token, user);
 		passwordResetTokenRepository.save(myToken);
 	}
 	
+	@Override
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	
+	public User findByEmail (String email) {
+		return userRepository.findByEmail(email);
+	}
+	
+	public User createUser(User user, Set<UserRole> userRoles) throws Exception{
+		User localUser = userRepository.findByUsername(user.getUsername());
+		
+		if(localUser != null) {
+			LOG.info("user {} already exists. ", user.getUsername());
+		} else {
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			
+			user.getUserRoles().addAll(userRoles);
+			
+			localUser = userRepository.save(user);
+		}
+		
+		return localUser;
+	}
+	
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
 
 }
